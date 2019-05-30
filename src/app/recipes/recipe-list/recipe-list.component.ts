8169ab4deb2b2ce589102720d7c1e286e550daca
tosetod <tose_todorovski@yahoo.com>
2019-05-30 16:29:06 +0200

    First angular project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test','https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg'),
+    new Recipe('Test Recipe', 'This is simply a test','https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg')
+  ];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model';
+
+@Component({
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit{
+    ingridients: Ingridient[] = [
+        new Ingridient('Apples', 5),
+        new Ingridient('Tomatoes', 10)
+    ];
+
+    constructor(){ }
+
+    ngOnInit(){
+    }
+}
